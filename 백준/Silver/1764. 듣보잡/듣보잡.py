a5d3a82e313e1792b3e432f450849954086ca6db
Soohyun <91974779+ocean010315@users.noreply.github.com>
2024-08-15 00:08:12 +0900

    [Silver IV] Title: 듣보잡, Time: 2340 ms, Memory: 44228 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -3,5 +3,6 @@ N, M = [int(i) for i in input().split()]
 neverHeard = set([input() for n in range(N)])
 neverSeen = set([input() for m in range(M)])
 
+never = sorted(list(neverHeard & neverSeen))
 print(len(neverHeard&neverSeen))
-[print(name) for name in sorted(list(neverHeard & neverSeen))]
+[print(name) for name in never]
